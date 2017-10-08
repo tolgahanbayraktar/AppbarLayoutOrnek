60445ebb548c6b6409e0e26de06c7ea5146e8ae2
Tolgahan <tolgahanbayraktar@gmail.com>
2017-10-08 23:13:08 +0300

    Initial commit

---
+++ app/src/main/java/com/tbdev/appbarlayoutornek/MainActivity.java
@@ -0,0 +1,19 @@
+package com.tbdev.appbarlayoutornek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setTitle("App Bar Layout");
+        toolbar.inflateMenu(R.menu.menu_main);
+    }
+}
